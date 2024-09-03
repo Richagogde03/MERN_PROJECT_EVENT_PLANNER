ae9569601b117e26464c8dd7a542125f329561b9
Richagogde03 <115970576+Richagogde03@users.noreply.github.com>
2024-09-03 18:47:16 +0530

    Update Contact.jsx

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
     await axios
     .post(
-      "http://localhost:4000/api/v1/message/send", 
+      "https://mern-project-backend-b25b.onrender.com", 
       {
         name,
         email,
